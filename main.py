9bdb9b1175f13e4b60a32b9917e65556303d71ef
Gabe Clow <gabejclow@gmail.com>
2023-08-10 21:18:55 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Starting Main")
+
+class bird:
+    def __init__(self,primaryColor="white",wingspan=0):
+        self.primaryColor = primaryColor
+        self.wingspan = wingspan
+
+BigBird = bird()
+print(BigBird.primaryColor, BigBird.wingspan)
+
+
+
+
+
+print("completed")
